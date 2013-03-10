7ae8cf01d357f9fa47e56a0445ca5b44800312d9
Jaume <Jaume@Jaume-PC>
2013-03-10 18:42:25 +0100

    Dejando el proyecto preparado y listo para comenzar: como debe ser el mainActivity y una propiedad en el manifest muy necesaria para que funcione todo el motor.

--- src/com/towerdefense/MainActivity.java
+++ src/com/towerdefense/MainActivity.java
@@ -1,22 +1,39 @@
 package com.towerdefense;
 
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.BaseGameActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseGameActivity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onCreateResources(OnCreateResourcesCallback arg0)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onCreateScene(OnCreateSceneCallback arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPopulateScene(Scene arg0, OnPopulateSceneCallback arg1)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
