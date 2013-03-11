c91ca62f03bb9d2ff23b3bca244f6f42234edba7
ruben <ruben@rubenS>
2013-03-11 17:04:16 +0100

    Definición básica para cargar la activity

--- src/com/towerdefense/MainActivity.java
+++ src/com/towerdefense/MainActivity.java
@@ -1,39 +1,54 @@
 package com.towerdefense;
 
+import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.scene.background.modifier.ColorBackgroundModifier;
 import org.andengine.ui.activity.BaseGameActivity;
+import org.andengine.util.color.Color;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends BaseGameActivity {
+	final static int HEIGHT = 480;
+	final static int WIDTH = 800;
+	Camera camera = new Camera(0, 0, HEIGHT, WIDTH);
 
 	@Override
 	public EngineOptions onCreateEngineOptions() {
 		// TODO Auto-generated method stub
-		return null;
+
+		EngineOptions options = new EngineOptions(true,
+				ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(
+						WIDTH, HEIGHT), camera);
+		return options;
 	}
 
 	@Override
 	public void onCreateResources(OnCreateResourcesCallback arg0)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
+		arg0.onCreateResourcesFinished();
 	}
 
 	@Override
 	public void onCreateScene(OnCreateSceneCallback arg0) throws Exception {
 		// TODO Auto-generated method stub
-		
+		Scene scene = new Scene();
+		scene.setBackground(new Background(0, 255, 0));
+		arg0.onCreateSceneFinished(scene);
 	}
 
 	@Override
 	public void onPopulateScene(Scene arg0, OnPopulateSceneCallback arg1)
 			throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
